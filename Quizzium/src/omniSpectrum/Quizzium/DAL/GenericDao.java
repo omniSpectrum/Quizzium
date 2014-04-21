df52de70ea0668de8c6e50579332c78543b6d148
Daniel Freitas <danfreitaschn+programming@gmail.com>
2014-04-22 02:17:51 +0300

    Started Generic DAO implementation using hibernate

--- Quizzium/src/omniSpectrum/Quizzium/DAL/GenericDao.java
+++ Quizzium/src/omniSpectrum/Quizzium/DAL/GenericDao.java
@@ -3,14 +3,60 @@ package omniSpectrum.Quizzium.DAL;
 import java.io.Serializable;
 import java.util.List;
 
-public interface GenericDao<T, PK extends Serializable> {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public abstract class GenericDao<T, PK extends Serializable> implements IGenericDao<T, PK> {
+
+	private SessionFactory sessionFactory;
 	
-	PK save(T newInstance);
-	void update(T transientObject);
-	void saveOrUpdate(T transientObject);
-	void delete (T persistentObject);
-	T findById(PK id);
-	List<T> findAll();
-	List<T> findByProperty(String propertyName, Object value);
+	protected final Session getCurrentSession(){
+		return sessionFactory.getCurrentSession();
+	}
+	
+	@Override
+	public PK save(T newInstance) {
+		
+		return (PK) getCurrentSession().save(newInstance);
+	}
+
+	@Override
+	public void update(T transientObject) {
+		// TODO Auto-generated method stub
+		getCurrentSession().update(transientObject);
+		
+	}
+
+	@Override
+	public void saveOrUpdate(T transientObject) {
+		// TODO Auto-generated method stub
+		getCurrentSession().saveOrUpdate(transientObject);
+		
+	}
+
+	@Override
+	public void delete(T persistentObject) {
+		// TODO Auto-generated method stub
+		getCurrentSession().delete(persistentObject);
+		
+	}
+
+	@Override
+	public T findById(PK id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<T> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<T> findByProperty(String propertyName, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
