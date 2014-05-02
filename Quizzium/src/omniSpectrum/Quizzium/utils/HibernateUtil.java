cce6ab4a902c006ae9cc6bfd710300c3a23743d3
Ytseboy <danfreitaschn+programming@gmail.com>
2014-05-02 21:26:32 +0300

    Removed deprecated methods

--- Quizzium/src/omniSpectrum/Quizzium/utils/HibernateUtil.java
+++ Quizzium/src/omniSpectrum/Quizzium/utils/HibernateUtil.java
@@ -1,16 +1,21 @@
 package omniSpectrum.Quizzium.utils;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil {
-	private static final SessionFactory sessionFactory = buildSessionFactory();
-	
+	private static SessionFactory sessionFactory = buildSessionFactory();
+	private static ServiceRegistry serviceRegistry;
 	
 	private static SessionFactory buildSessionFactory(){
 		try{
-			SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-			
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 			return sessionFactory;
 		} catch (Throwable ex){
 			System.err.println("SessionFactory creation failed" + ex);
