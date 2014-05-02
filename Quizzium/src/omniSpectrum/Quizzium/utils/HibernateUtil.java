b5e1e5b4fba3eaa8c1a337fffc3c417f1cf00a6f
Ytseboy <danfreitaschn+programming@gmail.com>
2014-05-02 21:28:38 +0300

    Updated Hibernate util to remove deprecated classes

--- Quizzium/src/omniSpectrum/Quizzium/utils/HibernateUtil.java
+++ Quizzium/src/omniSpectrum/Quizzium/utils/HibernateUtil.java
@@ -4,7 +4,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil {
 	private static SessionFactory sessionFactory = buildSessionFactory();
